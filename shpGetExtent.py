1a54a36656324f02b4036add9f70c0413c54bf37
Laguna Du <lag945@gmail.com>
2020-12-24 10:34:36 +0800

    換py

--- shpGetExtent.py
+++
@@ -1,9 +0,0 @@
-from osgeo import ogr
-import os
-
-# Get a Layer's Extent
-inShapefile = "A.SHP"
-inDriver = ogr.GetDriverByName("ESRI Shapefile")
-inDataSource = inDriver.Open(inShapefile, 0)
-inLayer = inDataSource.GetLayer()
-extent = inLayer.GetExtent()
